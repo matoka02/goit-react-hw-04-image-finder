6c9e6ec17609a0ab8685e353a450c056e74219c3
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-07-12 15:18:13 +0300

    update

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,25 +1,14 @@
 import propTypes from 'prop-types';
 import css from './Button.module.css';
 
-export const Button = ({ onClick }) => (
+const Button = ({ onClick }) => (
   <button className={css.Button} onClick={onClick} type="button">
     Load more
   </button>
 );
+
 Button.propTypes = {
   onClick: propTypes.func.isRequired,
 };
 
-
-// import propTypes from 'prop-types';
-// import css from './Button.module.css';
-
-// const Button = ({onClick}) => (
-//   <button className={css.Button} onClick={onClick} type="button">Load more</button>
-// );
-
-// Button.propTypes = {
-//   onClick: propTypes.func.isRequired,
-// };
-
-// export default Button;
+export default Button;
