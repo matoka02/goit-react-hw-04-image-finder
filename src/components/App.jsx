8cd413fda8376f4faecf111d37e028b37c5283d6
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-07-12 20:15:26 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import ImageGallery from './ImageGallery/ImageGallery';
 import Loader from './Loader/Loader';
 import Modal from './Modal/Modal';
 import Searchbar from './Searchbar/Searchbar';
-import { fetchImages } from './api/fetchImages';
+import fetchImages from './api/fetchImages';
 
 export class App extends Component {
   state = {
