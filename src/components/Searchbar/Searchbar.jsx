7ff10ead04c6b50ce8c27958f0df14db5f95a17a
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-07-12 16:51:41 +0300

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,10 +1,12 @@
 import propTypes from 'prop-types';
+import {CiSearch} from 'react-icons/ci'
 import css from './Searchbar.module.css';
 
-export const Searchbar = ({ onSubmit }) => (
+const Searchbar = ({ onSubmit }) => (
   <header className={css.Searchbar}>
     <form className={css.SearchForm} onSubmit={onSubmit}>
       <button type="submit" className={css.SearchFormButton}>
+        <CiSearch />
         <span className={css.SearchFormButtonLabel}>Search</span>
       </button>
 
@@ -24,30 +26,4 @@ Searchbar.propTypes = {
   onSubmit: propTypes.func,
 };
 
-
-// import propTypes from 'prop-types';
-// import css from './Searchbar.module.css';
-
-// const Searchbar = ({ onSubmit }) => {
-//   <header className={css.Searchbar}>
-//     <form className={css.SearchForm} onSubmit={onSubmit}>
-//       <button className={css.SearchFormButton} type="submit">
-//         <span className={css.SearchFormButtonLabel}>Search</span>
-//       </button>
-//       <input
-//         className={css.SearchFormInput}
-//         type="text"
-//         name="inputForSearch"
-//         autoComplete="off"
-//         autoFocus
-//         placeholder="Search images and photos"
-//       />
-//     </form>
-//   </header>;
-// };
-
-// Searchbar.propTypes = {
-//   onSubmit: propTypes.func,
-// };
-
-// export default Searchbar;
+export default Searchbar;
